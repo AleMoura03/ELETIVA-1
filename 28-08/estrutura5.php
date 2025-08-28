9ba9b3badc774a1d5f418e05eb087b409fa76a25
AleMoura03 <ale_moura03@hotmail.com>
2025-08-28 20:37:37 -0300

    28-8.2

---
+++ 28-08/estrutura5.php
@@ -0,0 +1,10 @@
+<?php
+    include("cabecalho");
+
+    $i = 1;
+    do {
+        echo "<p>Número $i</p>";
+        $i++;
+    } while($i <= 5);
+    
+    include("rodape.php");
---
+++ 28-08/estrutura6.php
@@ -0,0 +1,8 @@
+<?php
+    include("cabecalho");
+
+    for($i=1; $i<=5; $i++){
+        echo "<p>Número $i</p>";
+    }
+    
+    include("rodape.php");
