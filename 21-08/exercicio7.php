2b60d0274fa2bfbf20c8b3514c36efb8fb983380
AleMoura03 <ale_moura03@hotmail.com>
2025-09-07 20:45:29 -0300

    07-09

--- 21-08/exercicio7.php
+++ 21-08/exercicio7.php
@@ -1,9 +1,31 @@
+<!doctype html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 7</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Exercício 7</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Digite a temperatura em ºF</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+           
+            <button type="submit" class="btn btn-primary">Converter</button>
+        </form>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $valor1 = $_POST["valor1"];
-            $multiplicar = ($valor1 - 32) / 1.8;
-            echo "<p> Convertendo: $valor1 ºF = $multiplicar ºC</p>";
+            $conversor = ($valor1 - 32) / 1.8;
+            $conversorFormatado = number_format($conversor,2,",",".");
+            echo "<p> Convertendo: $valor1 ºF = $conversorFormatado ºC</p>";
+            
         }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
---
+++ 21-08/exercicio8.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 8</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Exercício 8</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Digite a Altura</label>
+                <input type="number" id="altura" name="altura" class="form-control" required="">
+            </div>
+
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Digite a Largura</label>
+                <input type="number" id="largura" name="largura" class="form-control" required="">
+            </div>
+           
+            <button type="submit" class="btn btn-primary">Calcular Área</button>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $altura = $_POST["altura"];
+            $largura = $_POST["largura"];
+            $area = ($altura * $largura) / 2;
+            $areaFormatada = number_format($area,2,".","");
+            
+            echo "<p> A área do triângulo com largura $largura e altura $altura é: $areaFormatada ²</p>";
+
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
