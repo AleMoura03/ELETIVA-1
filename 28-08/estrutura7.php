2e63a7e64a5c3234c1097eed903ceb298f39bea2
AleMoura03 <ale_moura03@hotmail.com>
2025-09-23 18:22:18 -0300

    .

--- 28-08/estrutura7.php
+++ 28-08/estrutura7.php
@@ -1,7 +1,28 @@
 <?php
 include("cabecalho.php");
 ?>
+<form method="post">
+    <div class="mb-3">
 
+        <label for="valor1" class="form-label">Informe o número</label>
+        <input type="number" id="valor1" name="valor1" class="form-control" required="">
+
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $valor1 = $_POST['valor1'];
+        $i = 1;
+        $soma = 0;
+
+    while ($i <= $valor1){
+        $soma += $i;
+        $i++;
+    }
+    echo "<p>A soma de todos os números de 1 até $valor1 é: $soma</p>";
+}
+?>
 <?php
 include("rodape.php");
 ?>
--- 28-08/estrutura8.php
+++ 28-08/estrutura8.php
@@ -1,7 +1,27 @@
 <?php
 include("cabecalho.php");
 ?>
+<form method="post">
+    <div class="mb-3">
 
+        <label for="valor1" class="form-label">Informe o número</label>
+        <input type="number" id="valor1" name="valor1" class="form-control" required="">
+
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $valor1 = $_POST['valor1'];
+            echo "<p>Contagem regressiva: </p>";
+
+            do {
+                echo $valor1 . " ";
+                $valor1--;
+            } while ($valor1 >=1);
+
+        }
+?>
 <?php
 include("rodape.php");
 ?>
