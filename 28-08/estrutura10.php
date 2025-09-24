f57dd85f4cbd0db23a7942ca5596e2ec489b85d0
AleMoura03 <ale_moura03@hotmail.com>
2025-09-24 20:25:24 -0300

    ,

--- 28-08/estrutura10.php
+++ 28-08/estrutura10.php
@@ -5,7 +5,7 @@ include("cabecalho.php");
     <div class="mb-3">
         
         <label for="valor1" class="form-label">Informe o número</label>
-        <input type="number" id="valor1" name="valor1" class="form-control" required="">
+        <input type="number" id="valor1" name="valor1" class="form-control" required min="0">
         
     </div>
     <button type="submit" class="btn btn-primary">Enviar</button>
@@ -13,11 +13,12 @@ include("cabecalho.php");
     <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $valor1 = $_POST['valor1'];
-        echo "<p>A tabuada de $valor1 é: $tabuada</p>";
+        echo "<h3>A tabuada de $valor1 é: </h3>";
 
-        for($i=1;$i<=10;$i++){
+        for ($i = 1; $i <= 10; $i++){
             $tabuada = $valor1 * $i;
-            
+            echo "$valor1 x $i = $tabuada<br>";
+
         }
        
     }
