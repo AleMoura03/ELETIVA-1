21163eba5bdc5ee37014441da60174997f37f6ab
AleMoura03 <ale_moura03@hotmail.com>
2025-11-06 23:31:48 -0300

    .

---
+++ projetop2/banco.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS sistema_agendamento CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE sistema_agendamento;
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS paciente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20)
+);
+
+CREATE TABLEIF NOT EXISTS medico (
+    id INt AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    especialidade VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS horario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data DATE NOT NULL,
+    hora TIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS agendamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_paciente INT NOT NULL,
+    id_medico INT NOT NULL,
+    id_horario INT NOT NULL,
+    FOREIGN KEY (id_paciente) REFERENCES paciente(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_medico) REFERENCES medico(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_horario) REFERENCES horario(id) ON DELETE CASCADE
+);
